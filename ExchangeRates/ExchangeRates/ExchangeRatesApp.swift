1d026579b3c485520ea89794dfbd44616d5b8f69
Fabricio Bahiense <fabriciobs2000@gmail.com>
2023-10-18 10:52:30 -0400

    setting up project

---
+++ ExchangeRates/ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by user239336 on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
